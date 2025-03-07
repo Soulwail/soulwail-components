45c9baf0013422096518499c890fad411633f034
Soulwail <879457843@qq.com>
2025-03-07 17:02:15 +0800

    feat: 增加 ts 类型导出

--- packages/react-grid-layout/src/index.tsx
+++ packages/react-grid-layout/src/index.tsx
@@ -1,3 +1,4 @@
+import type { Layout, Layouts, ResponsiveGridProps } from './components/ResponsiveGridLayout';
 import { ResponsiveGridLayout } from './components/ResponsiveGridLayout';
 
-export { ResponsiveGridLayout };
+export { Layout, Layouts, ResponsiveGridLayout, ResponsiveGridProps };
