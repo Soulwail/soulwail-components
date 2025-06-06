fa4a020da90899682538c762b731a36035c3e974
Soulwail <879457843@qq.com>
2025-06-06 15:45:30 +0800

    fix(react-grid-layout): 修复 react-grid-layout 导出 api 没有 ts 类型

--- packages/react-grid-layout/src/index.ts
+++ packages/react-grid-layout/src/index.ts
@@ -1,6 +1,10 @@
-import GridLayout, { Responsive, WidthProvider } from 'react-grid-layout';
+import RGL from 'react-grid-layout';
 import type { Layout, Layouts, ResponsiveGridProps } from './components/ResponsiveGridLayout';
 import { ResponsiveGridLayout } from './components/ResponsiveGridLayout';
+
+const WidthProvider: typeof RGL.WidthProvider = RGL.WidthProvider;
+const Responsive: typeof RGL.Responsive = RGL.Responsive;
+const GridLayout: typeof RGL = RGL;
 
 export { GridLayout, Responsive, ResponsiveGridLayout, WidthProvider };
 export type { Layout, Layouts, ResponsiveGridProps };
