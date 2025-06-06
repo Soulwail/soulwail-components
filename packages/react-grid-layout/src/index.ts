d7c606bde8f6bf83cb3fe1b5964d04fe25a23ea6
Soulwail <879457843@qq.com>
2025-06-06 16:02:09 +0800

    fix(react-grid-layout): 修复 react-grid-layout 原生 api 导出问题

--- packages/react-grid-layout/src/index.ts
+++ packages/react-grid-layout/src/index.ts
@@ -2,9 +2,5 @@ import GridLayout, { Responsive, WidthProvider } from 'react-grid-layout';
 import type { Layout, Layouts, ResponsiveGridProps } from './components/ResponsiveGridLayout';
 import { ResponsiveGridLayout } from './components/ResponsiveGridLayout';
 
-type GridLayoutProps = typeof GridLayout;
-type WithProviderProps = typeof WidthProvider;
-type ResponsiveProps = typeof Responsive;
-
 export { GridLayout, Responsive, ResponsiveGridLayout, WidthProvider };
-export type { GridLayoutProps, Layout, Layouts, ResponsiveGridProps, ResponsiveProps, WithProviderProps };
+export type { Layout, Layouts, ResponsiveGridProps };
