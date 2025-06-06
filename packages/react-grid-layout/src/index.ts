cd03a6f27aa95a8725cff5f0b064b4dded85e707
Soulwail <879457843@qq.com>
2025-06-06 15:24:32 +0800

    feat(react-grid-layout): 增加 react-grid-layout 原生 api

--- packages/react-grid-layout/src/index.ts
+++ packages/react-grid-layout/src/index.ts
@@ -1,6 +1,6 @@
+import GridLayout, { Responsive, WidthProvider } from 'react-grid-layout';
 import type { Layout, Layouts, ResponsiveGridProps } from './components/ResponsiveGridLayout';
 import { ResponsiveGridLayout } from './components/ResponsiveGridLayout';
 
+export { GridLayout, Responsive, ResponsiveGridLayout, WidthProvider };
 export type { Layout, Layouts, ResponsiveGridProps };
-
-export { ResponsiveGridLayout };
